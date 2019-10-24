e8126ee1a340499191166e65f0838ae511b2e6ad
edgon <edgon85@gmail.com>
2019-10-23 23:45:20 -0600

    Agregando breadcrumbs dinamicamente

--- src/app/components/admin/shared/breadcrumb-admin/breadcrumb-admin.component.ts
+++ src/app/components/admin/shared/breadcrumb-admin/breadcrumb-admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumb-admin',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbAdminComponent implements OnInit {
 
-  constructor() { }
+  titulo: string;
+
+  constructor( private router: Router, private title: Title) {
+
+    this.getDataRoute().subscribe(
+      (resp) => {
+        this.titulo = resp.title;
+        this.title.setTitle('Casa e Imagen | ' + this.titulo);
+      }
+    );
+
+  }
 
   ngOnInit() {
+  }
+
+  // ===== Obtener la informacion del router en especicamente la data la data ===== //
+  // ===== version corta ===== //
+  getDataRoute() {
+    return this.router.events.pipe(
+      filter(evento => evento instanceof ActivationEnd),
+      filter((evento: ActivationEnd) => evento.snapshot.firstChild === null),
+      map((evento: ActivationEnd) => evento.snapshot.data)
+    );
   }
 
 }
